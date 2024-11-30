780eee2bc411c2a7182ea8f02be44f37220cd4fb
Afira <afiratahmali@gmail.com>
2024-11-30 10:25:37 +0800

    updated login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import postLogin from "../api/post-login.js";
 
 function LoginForm() {
+    const navigate = useNavigate();  
+
     const [credentials, setCredentials] = useState({
         username: "",
         password: "",
@@ -24,6 +27,7 @@ function LoginForm() {
                 credentials.password
             ).then((response) => {
                 window.localStorage.setItem("token", response.token);
+                navigate("/");
             });
         }
     };
@@ -34,7 +38,7 @@ function LoginForm() {
             <label htmlFor="username">Username:</label>
             <input
                 type="text"
-                id="username"
+                 id="username"
                 placeholder="Enter username"
                 onChange={handleChange}
             />
