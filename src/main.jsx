9c46c64ffb4daf96897e819199d88f713ab5ba38
Afira <afiratahmali@gmail.com>
2024-11-30 22:30:11 +0800

    added CSS to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./main.css"
 
-// Here we import out pages
+// Import Pages
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import ProjectPage from "./pages/ProjectPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
-// Here we import our components
+// Import Components
 import NavBar from "./components/NavBar.jsx";
 import { AuthProvider } from "./components/AuthProvider.jsx";
 
-// Here we create our router and tell it whats pages to render at what path
 const router = createBrowserRouter([
-  // These are the three routes!
   {
     path: "/",
     // Putting our NavBar as the main component will causes the children to render in the <Outlet />
