512eae7c439f1836a7288d52da6af66794415ca6
Afira <afiratahmali@gmail.com>
2024-12-07 23:33:37 +0800

    got the new project form working

--- src/api/post-project.js
+++ src/api/post-project.js
@@ -1,37 +1,46 @@
-async function postProject(title, description, game_type, goal, start_date, end_date) {
-    const url = `${import.meta.env.VITE_API_URL}/projects/`;
-    const token = window.localStorage.setItem("token")
+async function postProject(
+  title,
+  description,
+  game_type,
+  goal,
+  image,
+  is_open,
+  start_date,
+  end_date
+) {
+  const url = `${import.meta.env.VITE_API_URL}/projects/`;
+  const token = window.localStorage.setItem("token");
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authotization": `Token ${token}`,
-      },
-      body: JSON.stringify({
-        "title": title,
-        "description": description,
-        "game_type": game_type,
-        "goal": goal,
-        "image": "",
-        "is_open": true,
-        "start_date": start_date,
-        "end_date": end_date
-      }),
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+    body: JSON.stringify({
+      title: title,
+      description: description,
+      game_type: game_type,
+      goal: goal,
+      image: image,
+      is_open: is_open,
+      start_date: start_date,
+      end_date: end_date,
+    }),
+  });
+
+  if (!response.ok) {
+    const fallbackError = `Error creating project`;
+
+    const data = await response.json().catch(() => {
+      throw new Error(fallbackError);
     });
-  
-    if (!response.ok) {
-      const fallbackError = `Error creating project`;
-  
-      const data = await response.json().catch(() => {
-        throw new Error(fallbackError);
-      });
-  
-      const errorMessage = data?.detail ?? fallbackError;
-      throw new Error(errorMessage);
-    }
-  
-    return await response.json();
+
+    const errorMessage = data?.detail ?? fallbackError;
+    throw new Error(errorMessage);
   }
-  
-  export default postProject;
+
+  return await response.json();
+}
+
+export default postProject;
--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,112 +1,122 @@
 import { useState } from "react";
 
 import postProject from "../api/post-project";
+import { useNavigate } from "react-router-dom";
 
 function ProjectForm() {
-    const [projectDetails, setProjectDetails] = useState({
-        "title": "",
-        "description": "",
-        "game_type": "",
-        "goal": "",
-        "image": "",
-        "is_open": true,
-        "start_date": "",
-        "end_date": ""
-    });
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleChange = (event) => {
-        const { id, value } = event.target;
-        setProjectDetails((prevProjectDetails) => ({
-            ...prevProjectDetails,
-            [id]: value,
-        }))
-    };
+  const [projectDetails, setProjectDetails] = useState({
+    title: "",
+    description: "",
+    game_type: "",
+    goal: "",
+    image:
+      "https://variety.com/wp-content/uploads/2024/01/featured_videogame_SR_v3.jpg?w=910&h=511&crop=1",
+    is_open: true,
+    start_date: "",
+    end_date: "",
+  });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (
-            projectDetails.title &&
-            projectDetails.description &&
-            projectDetails.game_type &&
-            projectDetails.goal &&
-            projectDetails.image &&
-            projectDetails.is_open &&
-            projectDetails.start_date &&
-            projectDetails.end_date
-        ) {
-            postProject(
-                projectDetails.title,
-                projectDetails.description,
-                projectDetails.game_type,
-                projectDetails.goal,
-                projectDetails.image,
-                projectDetails.is_open,
-                projectDetails.start_date,
-                projectDetails.end_date
-            ).then((response) => {
-                console.log(response);
-            })
-        }
-    };
+  console.log(projectDetails);
 
-    return(
-        <form>
-            <div>
-                <label htmlFor="title">Title:</label>
-                <input 
-                    type="text"
-                    id="title"
-                    placeholder="Enter title"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="description">Description:</label>
-                <input 
-                    type="text"
-                    id="description"
-                    placeholder="Enter description"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="game_type">Game Type:</label>
-                <select id="game_type" onChange={handleChange}>
-                    <option value="video">Video Game</option>
-                    <option value="tabletop">Tabletop Game</option>
-                    <option value="mobile">Mobile Game</option>
-                    <option value="cards">Playing Cards</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor="goal">Goal:</label>
-                <input 
-                    type="number"
-                    id="goal"
-                    placeholder="Enter goal amount"
-                    min="1"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="start_date">Start Date:</label>
-                <input 
-                    type="date"
-                    id="image"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="end_date">End Date:</label>
-                <input 
-                    type="date"
-                    id="image"
-                    onChange={handleChange}
-                />
-            </div>
-            <button type="submit" onClick={handleSubmit}>Create</button>
-        </form>
-    )
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setProjectDetails((prevProjectDetails) => ({
+      ...prevProjectDetails,
+      [id]: value,
+    }));
+  };
+
+  console.log(projectDetails);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    if (
+      projectDetails.title &&
+      projectDetails.description &&
+      projectDetails.game_type &&
+      projectDetails.goal &&
+      projectDetails.image &&
+      projectDetails.is_open &&
+      projectDetails.start_date &&
+      projectDetails.end_date
+    ) {
+      postProject(
+        projectDetails.title,
+        projectDetails.description,
+        projectDetails.game_type,
+        projectDetails.goal,
+        projectDetails.image,
+        projectDetails.is_open,
+        projectDetails.start_date,
+        projectDetails.end_date
+      )
+        .then((response) => {
+          console.log(response);
+          navigate("/");
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          window.alert(error.message);
+        });
+    }
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Enter title"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          id="description"
+          placeholder="Enter description"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="game_type">Game Type:</label>
+        <select id="game_type" onChange={handleChange}>
+          <option value="video">Video Game</option>
+          <option value="tabletop">Tabletop Game</option>
+          <option value="mobile">Mobile Game</option>
+          <option value="cards">Playing Cards</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="goal">Goal:</label>
+        <input
+          type="number"
+          id="goal"
+          placeholder="Enter goal amount"
+          min="1"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="start_date">Start Date:</label>
+        <input type="date" id="start_date" onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="end_date">End Date:</label>
+        <input type="date" id="end_date" onChange={handleChange} />
+      </div>
+      <button type="submit" onClick={handleSubmit} disabled={isLoading}>
+        {isLoading ? "Creating..." : "Create"}
+      </button>
+    </form>
+  );
 }
 
 export default ProjectForm;
