39257c96e5dfc0fe654c393029598091982e20c0
Afira <afiratahmali@gmail.com>
2024-12-21 11:57:08 +0800

    updated sign up form structure and styling, and updated global link styling

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import postUser from "../api/post-user";
 import postLogin from "../api/post-login.js";
 import { useAuth } from "../hooks/use-auth.js";
+
+import "./SignUpForm.css";
 
 function SignUpForm() {
   const navigate = useNavigate();
@@ -67,56 +69,62 @@ function SignUpForm() {
   };
 
   return (
-    <form>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          placeholder="Enter username"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="first_name">First Name:</label>
-        <input
-          type="text"
-          id="first_name"
-          placeholder="Enter first name"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="last_name">Last Name:</label>
-        <input
-          type="text"
-          id="last_name"
-          placeholder="Enter last name"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Enter email"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          placeholder="Enter password"
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit" onClick={handleSubmit} disabled={isLoading}>
-        {isLoading ? "Signing Up..." : "Sign Up"}
-      </button>
-    </form>
+    <section className="signup-form">
+      <p>
+        Have an account? <Link to="/login">Login</Link>
+      </p>
+      <h1>SIGN UP</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="first_name">First Name:</label>
+          <input
+            type="text"
+            id="first_name"
+            placeholder="First Name"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="last_name">Last Name:</label>
+          <input
+            type="text"
+            id="last_name"
+            placeholder="Last Name"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit} disabled={isLoading}>
+          {isLoading ? "Signing Up..." : "Sign Up"}
+        </button>
+      </form>
+    </section>
   );
 }
 
