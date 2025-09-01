5c305699a629ada1074cb46b81836b0ffc5fe554
Afira <afiratahmali@gmail.com>
2025-09-01 22:24:47 +0800

    added css to the project page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -5,11 +5,20 @@ import useProject from "../hooks/use-project";
 import PledgeForm from "../components/PledgeForm";
 import rocketloading from "../assets/rocketloading.gif";
 
+import "./ProjectPage.css";
+
 function ProjectPage() {
   // Here we use a hook that comes for free in react router called `useParams` to get the id from the URL so that we can pass it to our useProject hook.
   const { id } = useParams();
   // useProject returns three pieces of info, so we need to grab them all here
   const { project, isLoading, error } = useProject(id);
+
+  const gameTypeChoices = {
+    video: "Video Game",
+    tabletop: "Tabletop Game",
+    mobile: "Mobile Game",
+    cards: "Playing Cards",
+  };
 
   if (isLoading) {
     return (
@@ -46,22 +55,35 @@ function ProjectPage() {
   };
 
   return (
-    <div>
-      <h2>{project.title}</h2>
-      <h3>Created at: {project.date_created}</h3>
-      <h3>{`Status: ${project.is_open}`}</h3>
-      <h3>Pledges:</h3>
-      <ul>
-        {project.pledges.map((pledgeData, key) => {
-          return (
-            <li key={key}>
-              ${pledgeData.amount} from {pledgeData.supporter} -{" "}
-              {pledgeData.comment}
-            </li>
-          );
-        })}
-      </ul>
-      <PledgeButton />
+    <div className="project-page-container">
+      <section className="project-details">
+        <h1>{project.title}</h1>
+        <p>Subtitle here</p>
+        <p>{gameTypeChoices[project.game_type]}</p>
+        <img src={project.image} />
+        <p>test</p>
+        <progress value="1000" max={project.goal} />
+      </section>
+      <div className="description-pledges-container">
+        <section className="project-description">
+          <h2>Description</h2>
+          <p>{project.description}</p>
+        </section>
+        <section className="project-pledges">
+          <PledgeButton />
+          <h3>Pledges:</h3>
+          <ul>
+            {project.pledges.map((pledgeData, key) => {
+              return (
+                <li key={key}>
+                  ${pledgeData.amount} from {pledgeData.supporter} -{" "}
+                  {pledgeData.comment}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
